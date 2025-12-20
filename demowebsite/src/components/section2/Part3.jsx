3eb7b0af2b239dc3bca81c4d3b3a66c662ea1c6e
dhruval1907 <dhruvaldalwadi1907@gmail.com>
2025-12-20 23:16:18 +0530

    demoprojects

--- demowebsite/src/components/section2/Part3.jsx
+++ demowebsite/src/components/section2/Part3.jsx
@@ -1,19 +1,29 @@
-import React from 'react'
+import React from "react";
 
 const Part4 = () => {
   return (
-    <div className='main'>
-      <h1 className='h1'>A few more facts about us in numbers.</h1>
-      <div className='part2'>
+    <div className="main">
+      <h1 className="h1">A few more facts about us in numbers.</h1>
+      <div className="part2">
         <div className="child">
-            
+          <h2 className="h2">12,000+</h2>
+          <h4 className="h4">Hours of play annually</h4>
         </div>
-        <div className="child"></div>
-        <div className="child"></div>
-        <div className="child"></div>
+        <div className="child">
+          <h2 className="h2">89%</h2>
+          <h4 className="h4">Player retention rate</h4>
+        </div>
+        <div className="child">
+          <h2 className="h2">1,200+</h2>
+          <h4 className="h4">Active members</h4>
+        </div>
+        <div className="child">
+          <h2 className="h2">125+</h2>
+          <h4 className="h4">Annual Tournarments</h4>
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Part4
+export default Part4;
