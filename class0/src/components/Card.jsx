c090c34f4687618abf214d2a363a55d2183423f3
dhruval1907 <dhruvaldalwadi1907@gmail.com>
2025-12-21 16:22:28 +0530

    deleted

--- class0/src/components/Card.jsx
+++ class0/src/components/Card.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 
-const Card = () => {
+const Card = (promps) => {
+    console.log(promps);
+    
   return (
     <div>
-      <div className="card w-[20%] h[10%] border-1 border-white text-center">
-        <img class="h-[200px] w-[200px] object-cover" src="https://images.unsplash.com/photo-1765470383293-af214e818d79?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
-        <h1>Sarthack Sharma</h1>
-        <h4>26</h4>
-        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Provident, fuga.</p>
+      <div className="card w-[100%] h[100%] border-1 border-white text-center flex items-center flex-col  rounded-xl">
+        <img class="h-[200px] w-[200px] object-cover  rounded-full m-5" src={promps.img} alt="" />
+        <h1 class="font-semibold text-2xl mt-2">{promps.user}</h1>
+        <h4 class="font-medium text-3xl m-4">{promps.age}</h4>
+        <p class="text-center font-semibold text-xl">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Provident, fuga.</p>
       </div>
     </div>
   )
