ae6eae65dafcb8ed609122626fd5cdbce26ef81d
dhruval1907 <dhruvaldalwadi1907@gmail.com>
2025-12-21 10:27:48 +0530

    project2

---
+++ Reactwork1/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1 class="text-8xl font-semibold bg-red-300">Hello</h1>
+      <h2 class="text-2xl bg-amber-500 p-10 ">hello from sheriyans</h2>
+    </div>
+  )
+}
+
+export default App
