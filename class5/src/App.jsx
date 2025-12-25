b01cadb232a94ffc2a6e5181db5442e95c338d5c
dhruval1907 <dhruvaldalwadi1907@gmail.com>
2025-12-25 21:18:45 +0530

    usestate

--- class5/src/App.jsx
+++ class5/src/App.jsx
@@ -1,23 +1,28 @@
 import React from "react";
-import { useState } from "react";
 
 const App = () => {
-  const [num, setnum] = useState(0)
   return (
-
-    <div className="h-screen w-full bg-zinc-300">
-      <h1>{num}</h1>
-      <button style={{fontWeight:"600",padding:"10px 10px"}} onClick={()=>{
-        setnum(num+1)
-      }}>Increase</button>
-      <button style={{fontWeight:"600",padding:"10px 10px"}} onClick={()=>{
-        setnum(num+2)
-      }}>Increase by 2 </button>
-      <button style={{fontWeight:"600",padding:"10px 10px"}} onClick={()=>{
-        setnum(num+5)
-      }}>Increase by 5</button>
+    <div>
+      <div
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          height: "100vh",
+          flexDirection: "column",
+        }}
+      >
+        <div>
+          <input type="radio" name="male" />
+          <label htmlFor="male"> Black</label>
+        </div>
+        <div>
+          <input type="radio" name="male" />
+          <label htmlFor="male"> White</label>
+        </div>
+      </div>
     </div>
-  )
+  );
 };
 
 export default App;
