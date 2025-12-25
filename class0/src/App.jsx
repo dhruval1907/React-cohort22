b7ab0ff62225a45e215997fd5923b7e024d3503b
dhruval1907 <dhruvaldalwadi1907@gmail.com>
2025-12-25 14:10:20 +0530

    react

--- class0/src/App.jsx
+++ class0/src/App.jsx
@@ -3,7 +3,7 @@ import Card from './components/Card'
 
 const App = () => {
   return (
-    <div className='h-screen w-full bg-zinc-800  '>
+    <div className='h-screen w-full bg-zinc-800    '>
       <Card/>
     </div>
   )
