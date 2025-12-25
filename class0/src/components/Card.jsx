c86554d9dbf5abaf33eff95ef4d72bbcc0aa1275
dhruval1907 <dhruvaldalwadi1907@gmail.com>
2025-12-25 11:25:57 +0530

    props

--- class0/src/components/Card.jsx
+++ class0/src/components/Card.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 
-const Card = () => {
+const Card = (props) => {
   return (
-    <div>
-      
+    <div className='w-70 h-70 bg-zinc-700 rounded-xl p-3'>
+      <h1 className='text-white font-semibold text-3xl '>{props.name}</h1>
+      <h4 className='text-white font-semibold text-3xl'>age:{props.age}</h4>
+      <h3 className='text-white font-semibold text-3xl wrap-break-word'>{props.skills}</h3> 
     </div>
   )
 }
