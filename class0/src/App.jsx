90dd12616b648b74cd3cfd6285bac8857d2867c8
dhruval1907 <dhruvaldalwadi1907@gmail.com>
2025-12-25 13:13:18 +0530

    conition rendering

--- class0/src/App.jsx
+++ class0/src/App.jsx
@@ -1,56 +1,12 @@
-import React from "react";
-import Card from "./components/Card";
+import React from 'react'
+import Card from './components/Card'
 
 const App = () => {
-  const user = [
-    {
-      name: "dhruval",
-      age: 21,
-      skills: ["React ", "Nodejs ", "html ", "css ", "javascript "],
-    },
-    {
-      name: "harsh",
-      age: 25,
-      skills: [ "html ", "css ", "javascript "],
-    },
-    {
-      name: "harsh",
-      age: 25,
-      skills: [ "html ", "css ", "javascript  "],
-    },
-    {
-      name: "harsh",
-      age: 25,
-      skills: [ "html ", "css  ", "javascript "],
-    },
-    {
-      name: "harsh",
-      age: 25,
-      skills: [ "html ", "css ", "javascript "],
-    },
-    {
-      name: "harsh",
-      age: 25,
-      skills: [ "html ", "css ", "javascript "],
-    },
-  ];
   return (
-    <div className="p-4 flex gap-5">
-      {user.map((user, indx) => (
-        <div
-          className="w-70 h-70 p-4  bg-zinc-300 font-semibold text-3xl wrap-break-word"
-          key={indx}
-        >
-          <h1>{user.name}</h1>
-          <h4>age:{user.age}</h4>
-          <p>skills : {user.skills}</p>
-        </div>
-      ))}
-
-      {/* <Card name = {user[0].name} age = {user[0].age}  skills = {user[0].skills}/>
-      <Card name = {user[1].name} age = {user[1].age} skills = {user[1].skills}/> */}
+    <div className='h-screen w-full bg-zinc-800  '>
+      <Card/>
     </div>
-  );
-};
+  )
+}
 
-export default App;
+export default App
--- class0/src/components/Card.jsx
+++ class0/src/components/Card.jsx
@@ -1,13 +1,70 @@
-import React from 'react'
+import React from "react";
+import { motion } from "motion/react";
+import { useRef } from "react";
 
-const Card = (props) => {
+const Card = () => {
+  const users = [
+    {
+      image:
+        "https://images.unsplash.com/photo-1642456074142-92f75cb84533?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      name: "spider-man",
+      p: "Lorem ipsum dolor sit amrta ",
+      likecount: "72.9k",
+      posts: "256",
+      views: "453k",
+    },
+    {
+      image:
+        "https://images.unsplash.com/photo-1708376368427-ede2b537d494?q=80&w=737&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      name: "marvel",
+      p: "Lorem ipsum dolor sit amrta ",
+      likecount: "7.9k",
+      posts: "26",
+      views: "43k",
+    },
+  ];
+
+  const constraintsRef = useRef(null);
   return (
-    <div className='w-70 h-70 bg-zinc-700 rounded-xl p-3'>
-      <h1 className='text-white font-semibold text-3xl '>{props.name}</h1>
-      <h4 className='text-white font-semibold text-3xl'>age:{props.age}</h4>
-      <h3 className='text-white font-semibold text-3xl wrap-break-word'>{props.skills}</h3> 
+    <div
+      ref={constraintsRef}
+      className="h-screen w-full flex flex-wrap p-4 gap-4 overflow-hidden"
+    >
+      {users.map((elem, indx) => (
+        <motion.div
+          drag
+          key={indx}
+          dragConstraints={constraintsRef}
+          className="h-[45%] w-[18%] bg-white rounded-xl overflow-hidden "
+        >
+          <img
+            className="h-[150px] w-full object-cover"
+            src={elem.image}
+            alt=""
+          />
+          <h4 className="p-4 font-semibold text-xl">{elem.name}</h4>
+          <p className="p-4 font-semibold text-xl">{elem.p}</p>
+          <div className="w-full h-[250px] flex items-center justify-around flex-col">
+            <div className="flex">
+              <div className="w-20 h-[250px] p-2">
+                <h1 className="font-semibold text-normal ">{elem.likecount}</h1>
+                <h4 className="opacity-[0.5] font-semibold ">Likes</h4>
+              </div>
+              <div className="w-20 h-[250px] p-2">
+                <h1 className="font-semibold text-normal ">{elem.posts}</h1>
+                <h4 className="opacity-[0.5] font-semibold ">Posts</h4>
+              </div>
+              <div className="w-20 h-[250px]  p-2">
+                <h1 className="font-semibold text-normal ">{elem.views}</h1>
+                <h4 className="opacity-[0.5] font-semibold ">Views</h4>
+              </div>
+            </div>
+            <button className="px-4 py-2 bg-red-500">Know more</button>
+          </div>
+        </motion.div>
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default Card
+export default Card;
