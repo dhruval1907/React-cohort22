a793d7cb55f108f7df4aa3c12d0c9731bed205f4
dhruval1907 <dhruvaldalwadi1907@gmail.com>
2025-12-26 23:07:11 +0530

    usestate

--- class5/src/App.jsx
+++ class5/src/App.jsx
@@ -1,11 +1,22 @@
-import React from 'react'
+import React, { useState } from "react";
 
 const App = () => {
-  return (
-    <div className='h-screen w-full bg-black'>
-      
-    </div>
-  )
-}
+  const [isShown, setisShown] = useState(false);
 
-export default App
+  return (
+    <div className="h-screen w-full bg-black text-white  p-3">
+      <div style={{
+        display:isShown?"none":"block"
+      }}>
+        <input
+          type="text"
+          placeholder="password"
+          className="p-2 border-1 border-white rounded-sm"
+        />
+        <button className="p-2 bg-zinc-600 rounded-full px-7 ">show </button>
+      </div>
+    </div>
+  );
+};
+
+export default App;
