f3854aac8d357f048e3fe22af87ff8ccbf11f21d
dhruval1907 <dhruvaldalwadi1907@gmail.com>
2025-12-26 20:02:33 +0530

    usestate

--- class5/src/App.jsx
+++ class5/src/App.jsx
@@ -1,27 +1,19 @@
 import React, { useState } from 'react'
 
 const App = () => {
-  const [isDark,setisDark] = useState(false);
-
-  function themeSwitcher(){
-    <div style={{
-      height : "100vh",
-      backgroundColor : isDark ? "#111" : "#fff",
-      color :  isDark ? "#111" : "#fff",
-      display : "flex",
-      alignItems : "center",
-      justifyItems : "center",
-    }}>
-      
-    </div>
-  }
-
+  const [isDark, setisDark] = useState(false)
   return (
-    <div>
-      <h1>{isDark ? "dark them":"light theme"}</h1>
-      <button onClick={(themeSwitcher)=>{
-        setisDark(!isDark)
-      }}>change theme </button>
+    <div style={{
+        backgroundColor:isDark?"#fff":"#111",
+        color:isDark?"#111":"#fff",
+        display:"flex",
+        alignItems:"center",
+        height:"100vh",
+        justifyContent:"center",
+    }}>
+      <h1>{isDark?"light theme":"dark theme "}</h1>
+      <button onClick={}>chnage theme </button>
+      
     </div>
   )
 }
