e77620601194e6e6ef9c404afd8a953a78f33c2b
dhruval1907 <dhruvaldalwadi1907@gmail.com>
2025-12-26 12:44:07 +0530

    usestate

--- class5/src/App.jsx
+++ class5/src/App.jsx
@@ -1,15 +1,13 @@
 import React, { useState } from 'react'
 
 const App = () => {
-
-  const [num, setnum] = useState(0)
-
+  const [isOn,isOff] = useState(false)
   return (
     <div>
-      <h1>{num}</h1>
-      <button onClick={()=>setnum(num+1)}>Increae</button>
-      <button onClick={()=>setnum(num-1)}>Decrease</button>
-      <button onClick={()=>setnum()}>Reset</button>
+      <h1>{isOn?"ON":"OFF"}</h1>
+      <button onClick={()=>isOff(!isOn)}>
+        {isOn?"ON":"OFF"}
+      </button>
     </div>
   )
 }
