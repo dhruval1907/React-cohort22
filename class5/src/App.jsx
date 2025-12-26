809eb734d5f991fb5220d13fd8d084a157034ae2
dhruval1907 <dhruvaldalwadi1907@gmail.com>
2025-12-26 23:29:40 +0530

    usestate

--- class5/src/App.jsx
+++ class5/src/App.jsx
@@ -1,20 +1,17 @@
 import React, { useState } from "react";
 
 const App = () => {
-  const [isShown, setisShown] = useState(false);
-
+  const [isShown, setisshown] = useState(false);
   return (
-    <div className="h-screen w-full bg-black text-white  p-3">
-      <div style={{
-        display:isShown?"none":"block"
-      }}>
-        <input
-          type="text"
-          placeholder="password"
-          className="p-2 border-1 border-white rounded-sm"
-        />
-        <button className="p-2 bg-zinc-600 rounded-full px-7 ">show </button>
-      </div>
+    <div className="h-screen w-full bg-black text-white p-5 ">
+      <input
+        type={isShown ? "text" : "password"}
+        className="p-2 border border-white rounded-sm 
+                   bg-white text-black placeholder:text-zinc-500"
+      />
+      <button className="p-2 bg-zinc-600 rounded-full px-7" onClick={()=>setisshown(!isShown)}>
+        {isShown ? "show" : "hide"}
+      </button>
     </div>
   );
 };
