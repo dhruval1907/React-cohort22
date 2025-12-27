5a7c8e46ab6e2112c36b90602e2f240fccb45cbf
dhruval1907 <dhruvaldalwadi1907@gmail.com>
2025-12-28 00:31:38 +0530

    form handeling

--- towaybinding/src/App.jsx
+++ towaybinding/src/App.jsx
@@ -1,11 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 
 const App = () => {
+
+  const [user,setUser] = useState[""]
+
+  const formHandle = (e) => {
+    e.preventDefault();
+    // console.log("hello ji");
+    
+  };
+
   return (
     <div className="h-screen w-full p-10 bg-black text-white">
-      <form>
-        <input type="text" placeholder="enter ur name" className="" />
-        <button>submit</button>
+      <form
+        onSubmit={(e) => {
+          formHandle(e);
+        }}
+      >
+        <input
+          onChange={}
+          type="text"
+          placeholder="enter ur name"
+          className="border-1 border-white rounded-sm "
+        />
+        <button className="px-3 py-1 bg-amber-600 rounded-xl ml-2">
+          submit
+        </button>
       </form>
     </div>
   );
