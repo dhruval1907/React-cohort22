02f0616a8bd7ac12e1bb0b0d644e137051f79f94
dhruval1907 <dhruvaldalwadi1907@gmail.com>
2025-12-27 23:43:11 +0530

    twowaybinding

---
+++ towaybinding/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <form>
+        <input type="text" placeholder='entre your name' />
+      </form>
+    </div>
+  )
+}
+
+export default App
