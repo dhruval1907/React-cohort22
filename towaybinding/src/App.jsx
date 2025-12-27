a683ee091604cb8a68e55fb4a5aabd4b0d4cfeb6
dhruval1907 <dhruvaldalwadi1907@gmail.com>
2025-12-27 23:46:55 +0530

    form handeling

--- towaybinding/src/App.jsx
+++ towaybinding/src/App.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 
 const App = () => {
-  const formHandler = () => {
-    
+  const formHandler = (e) => {
+    e.preventDefault();
+    console.log("form submitted");
   };
   return (
     <div>
-      <form>
+      <form onSubmit={(e)=>{ 
+        formHandler(e)
+      }}>
         <input type="text" placeholder="entre your name" />
         <button>submit</button>
       </form>
