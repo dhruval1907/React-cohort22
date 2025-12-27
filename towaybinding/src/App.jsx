f77991a3ad7e7d0815d91f3a273739b3dbeef8fb
dhruval1907 <dhruvaldalwadi1907@gmail.com>
2025-12-28 00:27:12 +0530

    lele

--- towaybinding/src/App.jsx
+++ towaybinding/src/App.jsx
@@ -1,36 +1,12 @@
-import React, { useState } from "react";
+import React from "react";
 
 const App = () => {
-  const [user, setuser] = useState("");
-  const [allusers, setallusers] = useState([]);
-  const formHandler = (e) => {
-    e.preventDefault();
-
-    setallusers([...allusers, user]);
-    setuser("");
-    console.log(allusers);
-  };
   return (
-    <div style={{color:"white"}}>
-      <form
-        onSubmit={(e) => {
-          formHandler(e);
-        }}
-      >
-        <input
-          onChange={(e) => {
-            setuser(e.target.value);
-          }}
-          type="text"
-          value={user}
-          placeholder="entre your name"
-          style={{ padding: "20px 20px" }}
-        />
-        <button style={{ padding: "20px 20px" }}>submit</button>
+    <div className="h-screen w-full p-10 bg-black text-white">
+      <form>
+        <input type="text" placeholder="enter ur name" className="" />
+        <button>submit</button>
       </form>
-      {allusers.map(function (elem) {
-        return <h1>{elem}</h1>
-      })}
     </div>
   );
 };
