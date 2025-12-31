07c02db64a77f869f7b0967da0f499fa2d416df4
dhruval1907 <dhruvaldalwadi1907@gmail.com>
2025-12-31 13:40:23 +0530

    axios

--- axios/src/App.jsx
+++ axios/src/App.jsx
@@ -1,11 +1,25 @@
-import React from 'react'
-
+import React from "react";
+import axios from "axios";
+import Card from "./components/Card";
 const App = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  async function getdata() {
+    const ans = await axios.get("https://picsum.photos/v2/list");
+    console.log(ans.data);
+  }
 
-export default App
+  return (
+    <div className="min-h-screen w-full bg-black">
+      <button
+        onClick={getdata}
+        className="px-10 mb-7 mt-5 py-3 rounded-full bg-blue-300 font-semibold "
+      >
+        Get User Data
+      </button>
+      <div className="h-[70vh] w-full p-5 bg-black flex flex-wrap gap-5">
+        a
+      </div>
+    </div>
+  );
+};
+
+export default App;
