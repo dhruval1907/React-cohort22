4e83fe61e13e8d4a4e8ad124985d8327518da860
dhruval1907 <dhruvaldalwadi1907@gmail.com>
2026-01-01 19:49:04 +0530

    usestare

--- class5usestate/src/App.jsx
+++ class5usestate/src/App.jsx
@@ -1,12 +1,11 @@
-import React, { useState } from "react";
+import React from 'react'
 
 const App = () => {
-  uses
   return (
     <div>
-      <h1></h1>
+      
     </div>
-  );
-};
+  )
+}
 
-export default App;
+export default App
