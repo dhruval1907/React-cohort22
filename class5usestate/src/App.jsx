c0f94d67fcdb5250155b232413923ad0e0dd5dfc
dhruval1907 <dhruvaldalwadi1907@gmail.com>
2026-01-01 21:20:46 +0530

    usestate

--- class5usestate/src/App.jsx
+++ class5usestate/src/App.jsx
@@ -1,12 +1,20 @@
-import React from 'react'
+import React, { useState } from "react";
 
 const App = () => {
-  return (
-    <div className='p-5 flex gap-4'>
-      <input className='border-2 border-black' type="text" placeholder='enter your passowrd' />
-      <button className='px-4 py-2 bg-red-300 text-white rounded-full'>show pass</button>
-    </div>
-  )
-}
+  const [pass, setpass] = useState(false);
 
-export default App
+  return (
+    <div className="p-5 flex gap-4">
+      <input
+        className="border-2 border-black"
+        type="text"
+        placeholder="enter your passowrd"
+      />
+      <button onClick={()=>setpass(!pass)}  className="px-4 py-2 bg-red-300 text-white rounded-full">
+        {pass ? "show" : "hide"}
+      </button>
+    </div>
+  );
+};
+
+export default App;
