9d36c5edabb1c0f91fe61bdbb9841c27415cdc43
dhruval1907 <dhruvaldalwadi1907@gmail.com>
2026-01-01 21:07:10 +0530

    usestate

--- class5usestate/src/App.jsx
+++ class5usestate/src/App.jsx
@@ -1,11 +1,13 @@
-import React from 'react'
+import React, { useState } from "react";
 
 const App = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  const [color, setcolor] = useState(false);
 
-export default App
+  return (
+    <div className="h-screen w-full bg-black flex items-center justify-center">
+      <div onMouseOver={()=>setcolor(true)}  className={`h-100 w-100 ${color ? "bg-red-300" : "bg-blue-300"}`}></div>
+    </div>
+  );
+};
+
+export default App;
