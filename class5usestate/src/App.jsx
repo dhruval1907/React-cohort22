a63c20cdc166bf9e91ada27e4ceab8d72e16fabd
dhruval1907 <dhruvaldalwadi1907@gmail.com>
2026-01-01 19:52:21 +0530

    usestate

--- class5usestate/src/App.jsx
+++ class5usestate/src/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 
 const App = () => {
+
   return (
-    <div>
-      
+    <div className='h-screen bg-black w-full p-5'>
+      <p className=''>0</p>
+      <button className='px-4 py-2 bg-red-300 text-white rounded-full'>get data</button>
     </div>
   )
 }
