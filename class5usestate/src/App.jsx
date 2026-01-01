6d60d5565300a7a3d1e847e5214ccc6b0d3f1a4b
dhruval1907 <dhruvaldalwadi1907@gmail.com>
2026-01-01 22:05:36 +0530

    usestate

--- class5usestate/src/App.jsx
+++ class5usestate/src/App.jsx
@@ -1,13 +1,20 @@
-import React from 'react'
+import React, { useState } from "react";
 
 const App = () => {
-  return (
-    <div className='flex items-center justify-center flex-col gap-5 h-screen w-full bg-black'>
-      <h1 className='text-2xl text-white font-bold'>color : white</h1>
-      <div className='h-100 w-100 bg-white rounded-2xl'></div>
-      <button className='px-4 py-2 bg-red-400 rounded-full font-bold'>change color</button>
-    </div>
-  )
-}
+  const [color, setcolor] = useState(false);
 
-export default App
+  return (
+    <div className={`flex items-center justify-center flex-col gap-5 h-screen w-full ${color ? "bg-blue-200" : "bg-sky-400"}`}>
+      <h1 className="text-2xl text-white font-bold">color : {color ? "black" : "white"}</h1>
+      <div className={`h-100 w-100 ${color ? "bg-black" : "bg-white"} rounded-2xl`}></div>
+      <button
+        onClick={() => setcolor(!color)}
+        className="px-4 py-2 bg-red-400 rounded-full font-bold"
+      >
+        change color
+      </button>
+    </div>
+  );
+};
+
+export default App;
