77c6b16f5efe6c9c86004e72c8fa10a0b05b36cc
dhruval1907 <dhruvaldalwadi1907@gmail.com>
2026-01-01 20:33:23 +0530

    usestate

--- class5usestate/src/App.jsx
+++ class5usestate/src/App.jsx
@@ -4,8 +4,9 @@ const App = () => {
   const [val, setval] = useState({username : "harsh" , isLoggined : true})
   return (
     <div className='bg-red-300 h-screen w-full text-white font-semibold p-10'>
-      <p>username  :  {val.name}</p>
-      <p>isBanned  : {val.isBanned.toString()}</p>
+      <p>username  :  {val.username}</p>
+      <p>isBanned  : {val.isLoggined.toString()}</p>
+      <button className='px-4 py-2 bg-blue-300 rounded-full'>change</button>
       
     </div>
   )
