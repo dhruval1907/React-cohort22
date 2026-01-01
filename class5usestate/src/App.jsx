c064e3298e9f0edf8b851449412096496bf04b42
dhruval1907 <dhruvaldalwadi1907@gmail.com>
2026-01-01 20:53:20 +0530

    usestate

--- class5usestate/src/App.jsx
+++ class5usestate/src/App.jsx
@@ -1,12 +1,21 @@
 import React, { useState } from "react";
 
 const App = () => {
-  const [val, setval] = useState({ username: "harsh", isBlocked: "Blocked" });
+  const [val, setval] = useState({ username: "harsh", isBlocked: false });
   return (
     <div className="bg-red-300 h-screen w-full text-white font-semibold p-10">
       <p>username : {val.username}</p>
-      <p>isBanned : {val.isBlocked}</p>
-      <button onClick={()=>setval({...val,isBlocked : ""})} className={`px-3 py-2 rounded-full ${val.isBlocked ? "bg-blue-500" : "bg-red-500"}`}>Unblock</button>
+      <p>Login : {val.isBlocked ? "Logged in" : "Logged out" }</p>
+      <button
+        onClick={() =>
+          setval({ ...val, isBlocked: !val.isBlocked })
+        }
+        className={`px-3 py-2 rounded-full ${
+          val.isBlocked ? "bg-blue-500" : "bg-red-500"
+        }`}
+      >
+        Unblock
+      </button>
     </div>
   );
 };
