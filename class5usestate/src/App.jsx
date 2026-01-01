a2fcc3720a2ee11d72d3fb1c7e19065e24042155
dhruval1907 <dhruvaldalwadi1907@gmail.com>
2026-01-01 21:02:44 +0530

    usestate

--- class5usestate/src/App.jsx
+++ class5usestate/src/App.jsx
@@ -1,13 +1,9 @@
-import React, { useState } from 'react'
+import React from 'react'
 
 const App = () => {
-
-  useState
-
   return (
-    <div className='h-screen bg-black w-full text-white font-semibold flex flex-col gap-5  items-center justify-center'>
-      <p>Dark mode</p>
-      <button className='px-3 py-2 bg-red-300 rounded-full '>change mode</button>
+    <div>
+      
     </div>
   )
 }
