8e37bd00b3e15ffc9b7b7105543dff25be61f04f
dhruval1907 <dhruvaldalwadi1907@gmail.com>
2026-01-01 19:58:07 +0530

    usestate

--- class5usestate/src/App.jsx
+++ class5usestate/src/App.jsx
@@ -1,11 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const App = () => {
 
+  const [num, setnum] = useState({name : "harsh",isBanned : true})
+
   return (
     <div className='h-screen bg-black w-full p-5'>
-      <p className=''>0</p>
-      <button className='px-4 py-2 bg-red-300 text-white rounded-full'>get data</button>
+      <p className='text-white font-semibold mb-4 text-6xl'>name :{num.name}</p>
+      <p className='text-white font-semibold mb-4 text-6xl'>{num.isBanned.toString()}</p>
+      <button onClick={()=>setnum({name : "dhruval",isBanned:{}})}  className='px-4 py-2 bg-red-300 text-white rounded-full'>get data</button>
     </div>
   )
 }
