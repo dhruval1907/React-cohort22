1321ea2e3f00e5f1b005e7b55e87aef8b8b4e5fc
dhruval1907 <dhruvaldalwadi1907@gmail.com>
2026-01-01 09:40:18 +0530

    axios

--- axios/src/App.jsx
+++ axios/src/App.jsx
@@ -1,44 +1,25 @@
-import React, { useState } from "react";
 import axios from "axios";
+import React, { useState } from "react";
 import Card from "./components/Card";
-const App = () => {
-  const [allusers, setallusers] = useState([]);
 
-  async function getdata() {
-    const ans = await axios.get("https://picsum.photos/v2/list");
-    console.log(allusers);
-    setallusers(ans.data);
+const App = () => {
+
+  const [alluser, setalluser] = useState([])
+  const getdata = async()=>{
+    const response = await axios.get('https://picsum.photos/v2/list')
+    console.log(response.data);
+    setalluser(response.data)
   }
+  
 
   return (
-    <div className="min-h-screen w-full bg-black">
-      <button
-        onClick={getdata}
-        className="px-10 mb-7 mt-5 py-3 rounded-full bg-blue-300 font-semibold "
-      >
-        Get User Data
+    <div className="min-h-[100%] w-full flex flex-wrap items-start bg-black text-white p-10 gap-4">
+      <button onClick={getdata}  className="px-10 py-3 bg-white text-black font-semibold rounded-full">
+        Get data
       </button>
-      <div className="min-h-[70vh] w-full p-5 bg-black flex flex-wrap gap-5">
-        {allusers.map((elem, idx) => {
-          return (
-            <div key={idx} className="min-h-120 w-100 bg-red-500 rounded-2xl">
-              <div className="h-full w-full overflow-hidden relative z-[2]">
-                <img
-                  className="absolute  h-full w-full object-cover object-left rounded-2xl"
-                  // src={elem.author}
-                  alt=""
-                />
-                <div className="text-white p-5 absolute z-[3] bg-black/10 h-full w-full">
-                  <h1 className="font-semibold text-5xl">01.</h1>
-                  <h4 className="font-bold text-5xl mt-4 leading-[3rem]">
-                    Dhruval <br /> Dalwadi
-                  </h4>
-                </div>
-              </div>
-            </div>
-          );
-        })}
-      </div>
+      {alluser.map(function(elem,idx){
+        return <Card key={idx} name={elem.author} id={elem.id} image={elem.download_url}  />
+      })}
     </div>
   );
 };
--- axios/src/components/Card.jsx
+++ axios/src/components/Card.jsx
@@ -1,18 +1,18 @@
 import React from "react";
 
-const Card = () => {
+const Card = (props) => {
   return (
     <div className="h-120 w-100 bg-red-500 rounded-2xl">
       <div className="h-full w-full overflow-hidden relative z-[2]">
         <img
           className="absolute  h-full w-full object-cover object-left rounded-2xl"
-          src="https://images.unsplash.com/3/man-fishing.JPG?q=80&w=1332&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          src={props.image}
           alt=""
         />
         <div className="text-white p-5 absolute z-[3] bg-black/10 h-full w-full">
-          <h1 className="font-semibold text-5xl">01.</h1>
+          <h1 className="font-semibold text-5xl">{props.id}</h1>
           <h4 className="font-bold text-5xl mt-4 leading-[3rem]">
-            Dhruval <br /> Dalwadi
+           {props.name}
           </h4>
         </div>
       </div>
