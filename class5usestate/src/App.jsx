1a6d16b8823f6e09381c77260f67489c919efaca
dhruval1907 <dhruvaldalwadi1907@gmail.com>
2026-01-01 21:59:15 +0530

    usestate

--- class5usestate/src/App.jsx
+++ class5usestate/src/App.jsx
@@ -1,13 +1,23 @@
 import React, { useState } from "react";
 
 const App = () => {
-  const [color, setcolor] = useState([]);
+  const [isRed, setIsRed] = useState(false);
 
   return (
-    <div className="h-screen w-full bg-black flex flex-col items-center justify-center gap-5">
-      <div className={`h-100 w-100 bg-${color ? "bg-red-100" : "bg-blue-200"}`}></div>
-      <button onClick={()=>setcolor("red")} className="px-4 py-2 bg-white text-normal font-semibold ">
-        chnage color
+    <div className="h-screen w-full bg-black flex flex-col items-center justify-center gap-5 text-white">
+      <h1>{isRed ? "Red" : "Blue"}</h1>
+
+      <div
+        className={`h-[200px] w-[200px] ${
+          isRed ? "bg-red-300" : "bg-blue-300"
+        }`}
+      ></div>
+
+      <button
+        onClick={() => setIsRed(!isRed)}
+        className="px-4 py-2 bg-white text-black font-semibold"
+      >
+        Change Color
       </button>
     </div>
   );
