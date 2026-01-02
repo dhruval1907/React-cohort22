c0523850db32032c11f9da6427606e8ebeeb1540
dhruval1907 <dhruvaldalwadi1907@gmail.com>
2026-01-02 23:01:08 +0530

    the form

--- noteapp-twowaybinding/src/App.jsx
+++ noteapp-twowaybinding/src/App.jsx
@@ -15,7 +15,7 @@ const App = () => {
       <div className="w-[40%] h-[80%] bg-red-200">
         <div className="h-[10rem] w-full">
           <marquee behavior="scroll" direction="right">
-            <img className="h-[10rem]" src={image1} alt="" />
+            <img className="h-[15rem]" src={image1} alt="" />
           </marquee>
         </div>
         <form
@@ -23,12 +23,12 @@ const App = () => {
             // console.log(e);
             submitHandler(e);
           }}
-          className="flex items-start p-5    h-[100%]   flex-col gap-4"
+          className="flex items-start p-15    h-full   flex-col gap-4"
         >
           <input
             type="text"
             placeholder="Enter Task "
-            className="px-4 py-2 font-bold text-sm border-blue-200   border-2 rounded  w-full outline-none   "
+            className="px-4 py-4 font-bold text-sm border-blue-200   border-2 rounded  w-full outline-none   "
           />
           <textarea
             name=""
@@ -36,7 +36,7 @@ const App = () => {
             placeholder="Enter Details"
             className="px-4 h-32 font-bold text-sm border-blue-200   py-3  border-2 w-full rounded outline-none"
           ></textarea>
-          <button className="px-3 py-2   w-full bg-blue-500 outline-none  text-black font-bold rounded">
+          <button className="px-3 py-2   w-full bg-blue-500 outline-none  text-black font-bold rounded text-lg">
             add note
           </button>
         </form>
