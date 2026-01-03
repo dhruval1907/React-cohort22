1016b4e489231213dc3103cbc5264d1574e5e1f4
dhruval1907 <dhruvaldalwadi1907@gmail.com>
2026-01-03 18:02:39 +0530

    accessing the pages

--- react-routing/src/App.jsx
+++ react-routing/src/App.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
-import Home from './pages/Home'
-
+import {Route, Routes} from "react-router-dom";
+import About from './pages/About';
+import Courses from './pages/Courses';
 const App = () => {
   return (
-    <div className='h-screen w-full bg-black text-white'>
-      
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />}  />
+        <Route path='/about' element={<About/>} />
+        <Route path='/about' element={<Courses/>} />
+      </Routes>
     </div>
   )
 }
