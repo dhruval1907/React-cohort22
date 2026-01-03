e75297c9195ed062b26e66c2a2c002c7fe9311b5
dhruval1907 <dhruvaldalwadi1907@gmail.com>
2026-01-03 22:19:17 +0530

    starting

--- react-routing/src/App.jsx
+++ react-routing/src/App.jsx
@@ -1,12 +1,8 @@
 import React from 'react'
-import Nav from './components/Nav'
-import Home from './pages/Home'
 
 const App = () => {
   return (
-    <div className='h-screen w-full bg-black text-white font-semibold p-5'>
-      {/* <Nav/> */}
-      <Home/>
+    <div>
       
     </div>
   )
