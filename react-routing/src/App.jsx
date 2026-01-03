ca4040e6fb940a2eb84efd1c0149d7df58e1e47e
dhruval1907 <dhruvaldalwadi1907@gmail.com>
2026-01-03 18:50:26 +0530

    done

--- react-routing/src/App.jsx
+++ react-routing/src/App.jsx
@@ -1,17 +1,18 @@
-import React from 'react'
-import {Route, Routes} from "react-router-dom";
-import About from './pages/About';
-import Courses from './pages/Courses';
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import About from "./pages/About";
+import Courses from "./pages/Courses";
+import {Home} from "./pages/Home";
 const App = () => {
   return (
     <div>
       <Routes>
-        <Route path="/" element={<Home />}  />
-        <Route path='/about' element={<About/>} />
-        <Route path='/about' element={<Courses/>} />
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/about" element={<Courses />} />
       </Routes>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
