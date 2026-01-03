bfa641beff7c5fdbc42341222a9b1230427c010d
dhruval1907 <dhruvaldalwadi1907@gmail.com>
2026-01-03 14:06:06 +0530

    router

---
+++ react-routing/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Home from './pages/Home'
+
+const App = () => {
+  return (
+    <div className='h-screen w-full bg-black text-white'>
+      <Home/>
+    </div>
+  )
+}
+
+export default App
