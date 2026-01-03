23f58678a94ccc6e217d226da30e6c066dfe83bc
dhruval1907 <dhruvaldalwadi1907@gmail.com>
2026-01-04 00:19:21 +0530

    :perfomrming the routing

--- 01-website/src/App.jsx
+++ 01-website/src/App.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import Nav from './components/Nav'
 import Home from './pages/Home'
-
+import {Routes} from "react-router-dom";
 const App = () => {
   return (
     <div className='main p-10'>
       <Nav/>
-      <Home/>
+      <Routes path="/" element={<Home/>} />
     </div>
   )
 }
--- 01-website/src/pages/Home.jsx
+++ 01-website/src/pages/Home.jsx
@@ -1,23 +1,28 @@
 import React from "react";
-import image from "../assets/hero.png"
-import image2 from "../assets/HALLOWEEN.png"
-import image3 from "../assets/PRESENT.png"
-import image4 from "../assets/Shaoe.png"
+import image from "../assets/hero.png";
+import image2 from "../assets/HALLOWEEN.png";
+import image3 from "../assets/PRESENT.png";
+import image4 from "../assets/Shaoe.png";
+import reactange from "../assets/Rectangle.png";
 const Home = () => {
   return (
-    <div className="h-[85%] w-full flex items-center justify-center">
-      <div className="relative h-full w-full ">
+    <div className="h-[85%] w-full flex items-center justify-center relative ">
+      <div className=" h-full w-full ">
         <div className="absolute flex flex-col items-center w-full">
-            <div className="flex  relative w-full items-center justify-center h-40">
-                <img className="absolute" src={image3} alt="" />
-                <img className="ab" src={image4} alt="" />
-            </div>
-            <div className="text-xl absolute top-[13vh] right-[25%]">
-                <img className="w-[80%]" src={image2} alt="" />
-            </div>
+          <div className="flex  relative w-full items-center justify-center h-40">
+            <img className="absolute" src={image3} alt="" />
+            <img className="ab" src={image4} alt="" />
+          </div>
+          <div className="text-xl absolute top-[13vh] right-[25%]">
+            <img className="w-[80%]" src={image2} alt="" />
+          </div>
         </div>
         <div className="h-full w-full flex items-center justify-center ">
-            <img className="h-[100%] " src={image} alt="" />
+          <img className="h-[100%] " src={image} alt="" />
+          <div className="absolute z-[99]  h-60 w-[40%] flex flex-col items-center justify-center  bottom-0 left-[4%]" style={{backgroundImage:`URL(${reactange})`}}>
+            <h1 className="text-red-900 mb-5 font-bold text-3xl text-left w-[55%] ">HALLOWEEN NIGHT</h1>
+            <p className="text-white/60 font-light text-lg text-left w-[55%] ">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nulla qui unde quae iusto sunt ipsam omnis harum iure sapiente consequuntur molestiae facere.</p>
+          </div>
         </div>
       </div>
     </div>
