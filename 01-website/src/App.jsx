a20e7c15a165353270fd9b07e2ed369249c292a7
dhruval1907 <dhruvaldalwadi1907@gmail.com>
2026-01-04 03:26:15 +0530

    completed

--- 01-website/src/App.jsx
+++ 01-website/src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Nav from './components/Nav'
 import Home from './pages/Home'
-import {Route, Routes} from "react-router-dom";
+import {Route, Router, Routes} from "react-router-dom";
 import About from './pages/About';
 import Menu from './pages/Menu';
 import Contact from './pages/Contact';
@@ -15,6 +15,7 @@ const App = () => {
         <Route path='/about' element={<About/>}/>
         <Route path='/menu' element={<Menu />}/>
         <Route path='/contact' element={<Contact />}/>
+        <Route />
       </Routes>
     </div>
   )
