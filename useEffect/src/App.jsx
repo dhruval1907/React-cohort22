585b8076f769b8a3de0792a4a6ab1f3347e970a1
dhruval1907 <dhruvaldalwadi1907@gmail.com>
2026-01-03 13:26:45 +0530

    apicall

--- useEffect/src/App.jsx
+++ useEffect/src/App.jsx
@@ -10,46 +10,23 @@ const App = () => {
 
   return (
     <div>
-      <button
-        onClick={() => {
-          getData();
-        }}
-      >
-        clik me !!
-      </button>
+      <button onClick={getData}>clik me !!</button>
       <h1>{pokemon}</h1>
-      {pokemon.map(function (elm, idx) {
-        return <h1 key={idx}>{elm}</h1>;
+      {pokemon.map((elm, idx) => {
+        <h1 key={idx}>{elm.name}</h1>;
       })}
     </div>
   );
 };
 
 export default App;
+//       <button onClick={getData}>Click me !!</button>
 
+//       {pokemon.map((elm, idx) => (
+//         <h1 key={idx}>{elm.name}</h1>
+//       ))}
+//     </div>
+//   );
+// };
 
-    // import React, { useState } from "react";
-    // import axios from "axios";
-
-    // const App = () => {
-    //   const [pokemon, setpokemon] = useState([]);
-
-    //   const getData = async () => {
-    //     const response = await axios.get(
-    //       "https://pokeapi.co/api/v2/pokemon"
-    //     );
-    //     setpokemon(response.data.results); // ARRAY
-    //   };
-
-    //   return (
-    //     <div>
-    //       <button onClick={getData}>Click me !!</button>
-
-    //       {pokemon.map((elm, idx) => (
-    //         <h1 key={idx}>{elm.name}</h1>
-    //       ))}
-    //     </div>
-    //   );
-    // };
-
-    // export default App;
+// export default App;
