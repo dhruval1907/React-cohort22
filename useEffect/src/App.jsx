b56c3ff9a4deec8bd6bd31599ee2328a3e3132f0
dhruval1907 <dhruvaldalwadi1907@gmail.com>
2026-01-03 11:28:20 +0530

    useeffecr

--- useEffect/src/App.jsx
+++ useEffect/src/App.jsx
@@ -6,12 +6,12 @@ const App = () => {
   const getData = async () => {
     const response = await axios.get("https://pokeapi.co/api/v2/pokemon");
     console.log(response.data.results[0].name);
-    setpokemon(response.data.count);
+    setpokemon(response.data.results.name);
   };
 
   return (
     <div>
-      {/* {pokemon.map(function (elm, idx) {
+      {/* {pokemon.map(function(elm, idx) {
         return <h1 key={idx}>{elm}</h1>;
       })} */}
       <button
