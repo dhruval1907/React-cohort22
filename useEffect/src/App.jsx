ab2d50a8614c1808a2a793ba96653b4014fa8706
dhruval1907 <dhruvaldalwadi1907@gmail.com>
2026-01-03 11:25:47 +0530

    adding the data

--- useEffect/src/App.jsx
+++ useEffect/src/App.jsx
@@ -1,19 +1,28 @@
-import React from 'react'
-import axios from "axios"
+import React, { useState } from "react";
+import axios from "axios";
 const App = () => {
+  const [pokemon, setpokemon] = useState([]);
 
-  const getData = async()=>{
-    const response = await axios.get("https://pokeapi.co/api/v2/pokemon") 
-    console.log(response);
-    
-  }
-
+  const getData = async () => {
+    const response = await axios.get("https://pokeapi.co/api/v2/pokemon");
+    console.log(response.data.results[0].name);
+    setpokemon(response.data.count);
+  };
 
   return (
     <div>
-      
+      {/* {pokemon.map(function (elm, idx) {
+        return <h1 key={idx}>{elm}</h1>;
+      })} */}
+      <button
+        onClick={() => {
+          getData();
+        }}
+      >
+        clik me !!
+      </button>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
