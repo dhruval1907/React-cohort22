5e6419f181aa2c2c6d742b819e6d5bd28ee7d590
dhruval1907 <dhruvaldalwadi1907@gmail.com>
2026-01-06 23:36:04 +0530

    rounting

--- react-routing/src/components/Hero.jsx
+++ react-routing/src/components/Hero.jsx
@@ -1,11 +1,35 @@
-import React from 'react'
+import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const Hero = () => {
-  return (
-    <div className='h-[89.5%] w-full bg-zinc-400'>
-        <h1 className='text-3xl  text-center p-5 font-bold text-white'>This is a Home page </h1>
-    </div>
-  )
-}
+  const navigate = useNavigate();
 
-export default Hero
+  function btnCliked() {
+    navigate(-1)
+  }
+  function next() {
+    navigate(+1)
+  }
+
+  return (
+    <div className="h-[89.5%] w-full bg-zinc-400">
+      <h1 className="text-3xl  text-center p-5 font-bold text-white">
+        This is a Home page
+      </h1>
+      <button
+        onClick={btnCliked}
+        className="px-10 py-3 bg-blue-500 rounded-xl border-none text-white font-semibold"
+      >
+        Clike me
+      </button>
+      <button
+        onClick={next}
+        className="px-10 py-3 bg-blue-500 rounded-xl border-none text-white font-semibold"
+      >
+        Next page
+      </button>
+    </div>
+  );
+};
+
+export default Hero;
