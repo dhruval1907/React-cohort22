907ea53937a7ae337488a3611ac43b82aa6d0b76
dhruval1907 <dhruvaldalwadi1907@gmail.com>
2026-01-06 23:27:48 +0530

    routing

--- react-routing/src/components/Nav.jsx
+++ react-routing/src/components/Nav.jsx
@@ -1,16 +1,17 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Nav = () => {
   return (
-    <div className='h-10 bg-blue-500  p-10 text-white font-semibold text-xl flex justify-between items-center '>
+    <div className="h-10 bg-blue-500  p-10 text-white font-semibold text-xl flex justify-between items-center ">
       <h2>Sheriyans</h2>
-      <div className='flex items-center gap-10'>  
-        <a href="/">Home</a>
-        <a href="/about">About</a>
-        <a href="/Course">Course</a>
+      <div className="flex items-center gap-10">
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+        <Link to="/course">Course</Link>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Nav
+export default Nav;
