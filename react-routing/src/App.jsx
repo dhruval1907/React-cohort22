8363aadb4b9cbf3c51a13eb5e80744a26f95b949
dhruval1907 <dhruvaldalwadi1907@gmail.com>
2026-01-06 22:51:16 +0530

    routing

--- react-routing/src/App.jsx
+++ react-routing/src/App.jsx
@@ -2,10 +2,17 @@ import React from 'react'
 import Nav from './components/Nav'
 import Footer from './components/Footer'
 import Hero from './components/Hero'
-
+import {Route, Routes} from "react-router-dom";
 const App = () => {
   return (
     <div className='bg-black h-screen w-full'>
+
+        <Routes >
+          <Route path='/' element={<Hero />}/>
+          <Route path='/' element={<Hero />}/>
+          <Route path='/' element={<Hero />}/>
+        </Routes>
+
       <Nav/>
       <Hero />
       <Footer />
--- react-routing/src/components/Nav.jsx
+++ react-routing/src/components/Nav.jsx
@@ -2,9 +2,15 @@ import React from "react";
 
 const Nav = () => {
   return (
-      <div className="h-20 w-full text-white bg-gray-400">
-      <h1 className="text-3xl font-bold text-center p-5">this is a nav</h1>
-    </div>
+      <div className="h-20 w-full text-white flex items-center justify-between p-5  bg-gray-400">
+        <h2 className="font-semibold text-2xl ">Sheryians</h2>  
+        <div className="flex items-center gap-10 px-5">
+          <a className="font-semibold text-2xl cursor-pointer" href="">Home</a>
+          <a className="font-semibold text-2xl cursor-pointer" href="">About</a>
+          <a className="font-semibold text-2xl cursor-pointer" href="">Courses</a>
+          <a className="font-semibold text-2xl cursor-pointer" href="">Login</a>
+        </div>
+      </div>
   );
 };
 
