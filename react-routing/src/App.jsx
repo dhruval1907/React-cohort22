2ea91751c72e20671d5d2811621642555d125501
dhruval1907 <dhruvaldalwadi1907@gmail.com>
2026-01-06 22:44:46 +0530

    accesing that

--- react-routing/src/App.jsx
+++ react-routing/src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Nav from './components/Nav'
 import Footer from './components/Footer'
+import Hero from './components/Hero'
 
 const App = () => {
   return (
     <div className='bg-black h-screen w-full'>
       <Nav/>
+      <Hero />
       <Footer />
     </div>
   )
---
+++ react-routing/src/components/Hero.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <div className='h-[84%] w-full bg-gray-600 text-center p-5'>
+    <h1 className='text-white font-bold text-4xl '>This is a home page</h1>
+    </div>
+  )
+}
+
+export default Hero
