c667722e7168806ab6bafbb071e8464fccb53d84
dhruval1907 <dhruvaldalwadi1907@gmail.com>
2026-01-07 23:41:54 +0530

    dfghjkl

--- context-api/src/components/Nav.jsx
+++ context-api/src/components/Nav.jsx
@@ -2,7 +2,15 @@ import React from 'react'
 
 const Nav = () => {
   return (
-    <div>Nav</div>
+    <div className='flex  justify-between items-center w-full p-5 bg-blue-300/50 h-20'>
+        <h2 className=' text-2xl text-white font-semibold'>Myntra</h2>
+        <div className='flex gap-10 text-white font-bold text-xl'>
+            <a href="">Home</a>
+            <a href="">Products</a>
+            <a href="">Men </a>
+            <a href="">Woman</a>
+        </div>
+    </div>
   )
 }
 
