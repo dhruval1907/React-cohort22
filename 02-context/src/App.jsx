86a7ee0caec264d173d8be4b633f958416f2f73a
dhruval1907 <dhruvaldalwadi1907@gmail.com>
2026-01-08 22:10:07 +0530

    useeffect

--- 02-context/src/App.jsx
+++ 02-context/src/App.jsx
@@ -1,11 +1,23 @@
-import React from 'react'
+import React from "react";
+import axios from "axios";
 
 const App = () => {
+  const getData = async () => {
+    const response = await axios.get("https://fakestoreapi.com/products");
+    console.log(response.data);
+  };
   return (
     <div>
-      
+      <button
+        onClick={() => {
+          getData();
+        }}
+        className="h-7 w-30 rounded-2xl bg-blue-300 text-sm font-semibold"
+      >
+        get data
+      </button>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
