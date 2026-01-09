f52ee597d8a061091e01670beefa6c233bc4e93c
dhruval1907 <dhruvaldalwadi1907@gmail.com>
2026-01-10 00:28:35 +0530

    completed

--- 03-3dmacApple/src/App.jsx
+++ 03-3dmacApple/src/App.jsx
@@ -3,27 +3,44 @@ import { Canvas } from "@react-three/fiber";
 import { Environment, OrbitControls, ScrollControls } from "@react-three/drei";
 import Maccontaner from "./Maccontaner";
 
+
 const App = () => {
   return (
-    <div className="w-full h-screen ">
-      <div className="absolute items-center flex flex-col gap-2 text-center text-white top-32 left-1/2 -translate-x-1/2 ">
-        <h3 className="text-7xl font-semibold">macbook pro.</h3>
-        <h5>Oh so pro !</h5>
-        <p className="w-1/2">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda exercitationem provident eaque omnis ab adipisci!</p>
+    <div className="app">
+      
+      {/* NAVBAR */}
+      <nav className="navbar">
+        <ul>
+          <li>iPhone</li>
+          <li>iPad</li>
+          <li>Services</li>
+          <li>iOS</li>
+          <li>Mac</li>
+          <li>Products</li>
+        </ul>
+      </nav>
+
+      {/* HERO TEXT */}
+      <div className="hero-text">
+        <h1 className="gradient-text">macbook pro.</h1>
+        <h3>Oh so pro !</h3>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Quo sit recusandae pariatur laborum.
+        </p>
       </div>
+
       <Canvas camera={{ fov: 35, position: [0, 0, 6] }}>
-      <directionalLight position={[5, 5, 5]} intensity={1} />
-
-        {/* CONTROLS */}
-        {/* <OrbitControls target={[0, 0, 0]} /> */}
-
-        {/* ENVIRONMENT */}
+        <directionalLight position={[5, 5, 5]} intensity={1} />
         <Environment preset="studio" />
+
+      {/* <OrbitControls target={[0, 0, 0]} /> */}
 
         <ScrollControls pages={3}>
           <Maccontaner />
         </ScrollControls>
       </Canvas>
+
     </div>
   );
 };
