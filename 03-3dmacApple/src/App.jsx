8268484565faccf81ecc28e153c860c106aeb9cf
dhruval1907 <dhruvaldalwadi1907@gmail.com>
2026-01-09 23:53:31 +0530

    wrapping

--- 03-3dmacApple/src/App.jsx
+++ 03-3dmacApple/src/App.jsx
@@ -5,21 +5,26 @@ import Maccontaner from "./Maccontaner";
 
 const App = () => {
   return (
-    <Canvas camera={{ fov: 35, position: [0, 0, 6] }}>
-      {/* LIGHTS */}
-      {/* <ambientLight intensity={0.6} />
-      <directionalLight position={[5, 5, 5]} intensity={1} /> */}
+    <div className="w-full h-screen ">
+      <div className="absolute items-center flex flex-col gap-2 text-center text-white top-32 left-1/2 -translate-x-1/2 ">
+        <h3 className="text-7xl font-semibold">macbook pro.</h3>
+        <h5>Oh so pro !</h5>
+        <p className="w-1/2">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda exercitationem provident eaque omnis ab adipisci!</p>
+      </div>
+      <Canvas camera={{ fov: 35, position: [0, 0, 6] }}>
+      <directionalLight position={[5, 5, 5]} intensity={1} />
 
-      {/* CONTROLS */}
-      <OrbitControls target={[0, 0, 0]} />
+        {/* CONTROLS */}
+        {/* <OrbitControls target={[0, 0, 0]} /> */}
 
-      {/* ENVIRONMENT */}
-      <Environment preset="studio" />
+        {/* ENVIRONMENT */}
+        <Environment preset="studio" />
 
-      <ScrollControls pages={3}>
-        <Maccontaner />
-      </ScrollControls>
-    </Canvas>
+        <ScrollControls pages={3}>
+          <Maccontaner />
+        </ScrollControls>
+      </Canvas>
+    </div>
   );
 };
 
--- 03-3dmacApple/src/Maccontaner.jsx
+++ 03-3dmacApple/src/Maccontaner.jsx
@@ -13,6 +13,9 @@ const Maccontaner = () => {
 
   meshes.screen.rotation.x = THREE.MathUtils.degToRad(180);
   meshes.matte.material.map = tex;
+  meshes.matte.material.emissiveIntensity  = 0;
+  meshes.matte.material.metalness  = 0;
+  meshes.matte.material.roughness  = 1;
   let data = useScroll();
 
   useFrame((state, delta) => {
