d1ce8bce29cdfde0fde9941981bbc5a05f3a0d34
dhruval1907 <dhruvaldalwadi1907@gmail.com>
2026-01-09 23:32:58 +0530

    accesing that

--- 03-3dmacApple/src/Maccontaner.jsx
+++ 03-3dmacApple/src/Maccontaner.jsx
@@ -1,15 +1,18 @@
-import { useGLTF, useScroll } from "@react-three/drei";
+import { useGLTF, useScroll, useTexture } from "@react-three/drei";
 import { useFrame } from "@react-three/fiber";
 import React from "react";
 import * as THREE from "three";
 const Maccontaner = () => {
   let model = useGLTF("./mac.glb");
   let meshes = {};
+  let tex = useTexture("./red.jpg")
+
   model.scene.traverse((e) => {
     meshes[e.name] = e;
   });
 
   meshes.screen.rotation.x = THREE.MathUtils.degToRad(180);
+  meshes.matte.material.map = tex;
   let data = useScroll();
 
   useFrame((state, delta) => {
